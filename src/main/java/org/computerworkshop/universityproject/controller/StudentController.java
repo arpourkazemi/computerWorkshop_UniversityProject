f818ff04117bf5273aa9ef43e24c6ef58f89c32e
Amirreza <arpourkazemi@gmail.com>
2023-12-18 21:47:12 +0330

    add findAll and save functionality to controller

--- src/main/java/org/computerworkshop/universityproject/controller/StudentController.java
+++ src/main/java/org/computerworkshop/universityproject/controller/StudentController.java
@@ -1,11 +1,29 @@
 package org.computerworkshop.universityproject.controller;
 
+import lombok.AllArgsConstructor;
+import org.computerworkshop.universityproject.data.model.Student;
+import org.computerworkshop.universityproject.service.StudentLocalService;
+import org.computerworkshop.universityproject.service.StudentService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@AllArgsConstructor
 @RestController
 @RequestMapping("/api/v1/students")
 public class StudentController {
-    
+    private StudentLocalService studentLocalService;
+
+    @GetMapping()
+    public List<Student> findAll(){
+        return studentLocalService.findAll();
+    }
+
+    @PostMapping()
+    public Student save(Student student){
+        return studentLocalService.save(student);
+    }
 }
